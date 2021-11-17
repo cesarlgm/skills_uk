f89610a2aa22cb1cab8b462bcad5616103cb9f68
Cesar Garro-Marin <cesarlgm@bu.edu>
2021-11-17 16:16:56 -0500

    Joint maximization problem finished

--- code/process_SES/extract_solution.m
+++ code/process_SES/extract_solution.m
@@ -33,4 +33,28 @@ function [scale_array,scale_weight_array,index_weight_array,index_matrix_array]=
         index_weight_array{i}=index_weight_matrix;
         index_matrix_array{i}=index_matrix;
     end
+
+    %Extract average of solutions
+    mean_sol=mean(cell2mat(solution_array),2);
+
+    %First I extract all the elements of the solution
+    [scale_vector,scale_weights,index_weights]=split_parameters(mean_sol, ... 
+         n_skills,normalize_vector);
+
+    scale_matrix=create_output_scale_matrix(scale_vector,skill_data,n_skills,varnames);
+
+    scale_weight_matrix=extract_alpha(scale_weights,varnames);
+
+
+    index_weight_matrix=extract_weights(index_weights,n_educ);
+
+    weight_vector=transpose(table2array(scale_weight_matrix));
+    index_matrix=create_skill_index(scale_vector,weight_vector, ...
+        normalize_vector,scale_dummies,scale_mult_matrix,index_composition);
+
+    %Now I write the elements into the output array
+    scale_array{n_solutions+1}=scale_matrix;
+    scale_weight_array{n_solutions+1}=scale_weight_matrix;
+    index_weight_array{n_solutions+1}=index_weight_matrix;
+    index_matrix_array{n_solutions+1}=index_matrix;
 end
--- code/process_SES/minimization.m
+++ code/process_SES/minimization.m
@@ -21,9 +21,29 @@ tic
     n_solutions);
 toc
 
-% %%
-% %%EXTRACTING THE RESULTS
-% 
-% writetable(scale_matrix,'results/tables/scale_matrix.xls','WriteRowNames',true)
-% writetable(scale_weights,'results/tables/weight_on_scales.xls')
-% writetable(index_weights,'results/tables/weight_on_indexes.xls','WriteRowNames',true)
+%%
+%Compute average of solutions
+mean_MSE=mean(cell2mat(MSE_array));
+sd_MSE=std(cell2mat(MSE_array));
+
+
+
+ %%EXTRACTING THE RESULTS
+
+writetable(scale_array{51},'results/tables/scale_matrix.xls','WriteRowNames',true)
+writetable(alpha_array{51},'results/tables/weight_on_scales.xls')
+writetable(s_weights_array{51},'results/tables/weight_on_indexes.xls','WriteRowNames',true)
+
+s_weight_distance=zeros(n_solutions,1);
+scale_distance=zeros(n_solutions,1);
+alpha_distance=zeros(n_solutions,1);
+for i=1:n_solutions
+    dev=table2array(s_weights_array{i})-table2array(s_weights_array{n_solutions+1});
+    s_weight_distance(i,1)=max(abs(dev(:)));
+
+    dev=(table2array(alpha_array{i})-table2array(alpha_array{n_solutions+1}));
+    alpha_distance(i,1)=max(abs(dev(:)));
+
+    dev=(table2array(scale_array{i})-table2array(scale_array{n_solutions+1}));
+    scale_distance(i,1)=max(abs(dev(:)));
+end
