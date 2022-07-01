1609d39f2a3887f156f709a0dc0cdd2c29707c48
Cesar Garro-Marin <cesarlgm@bu.edu>
2022-06-30 21:22:40 -0400

    Creating matrix of instruments

---
+++ code/parameter_estimation/create_instrument_matrix.m
@@ -0,0 +1,10 @@
+function Z=create_instrument_matrix(data)
+    %Instruments for first equation are job by year dummies
+
+
+    %Instruments for second equation are the skills
+
+
+    %Instruments for third equation are 
+
+end
