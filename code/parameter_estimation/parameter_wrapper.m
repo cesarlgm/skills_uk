c396679d7851a193633173ab2350924eef4d9ea4
Cesar Garro-Marin <cesarlgm@bu.edu>
2023-04-27 13:52:44 -0400

    Added results of weighted occ FE

--- code/parameter_estimation/parameter_wrapper.m
+++ code/parameter_estimation/parameter_wrapper.m
@@ -110,7 +110,7 @@ b_rest=zeros(2,1);
 
 
 %%
-load("code/parameter_estimation/current_solution_weighted.mat",'solution');
+load("code/parameter_estimation/current_solution_weighted_FE.mat",'solution');
 
 
 [theta_matrix,comp_advg,pi]=extract_solution(solution,size_vector);
