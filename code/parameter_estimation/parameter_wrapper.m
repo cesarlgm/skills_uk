b33c66d21ea12ec261252809367626b8d035e52a
Cesar Garro-Marin <cesarlgm@bu.edu>
2023-05-06 14:50:17 -0600

    Redid parameter wrapper for two equation solution in the server

--- code/parameter_estimation/parameter_wrapper.m
+++ code/parameter_estimation/parameter_wrapper.m
@@ -3,43 +3,24 @@
 %%
 clear;
 
-%%
+%%READING DATA
 
 options = optimoptions('fmincon','Display','iter','MaxIterations',3000,'MaxFunctionEvaluations',2000000);
 
 
 
-cd 'C:/Users/thecs/Dropbox (Boston University)/boston_university/8-Research Assistantship/ukData';
+cd 'C:/Users/thecs/Dropbox (Boston University)/1_boston_university/8-Research Assistantship/ukData';
 addpath('code/parameter_estimation/','data');
-%% 
-
-
 
 data_path="data/additional_processing/gmm_example_dataset.csv";
-sol_path="data/additional_processing/initial_estimates.csv";
 
-%% 
 
 data=readtable(data_path);
-init_sol=readtable(sol_path);
 
-
-%%
-init_sol_vec=table2array(init_sol(:,'parameter'));
-
-    
-%%
 
 [z_matrix,y_matrix,s_matrix,n_total_parameters,size_vector,e1_dln_a_index,e1_educ_index, e1_code, ...
     lower_bound, upper_bound]= extract_data_matrices(data);
 
-
-%%
-
-error_solve=@(p)get_quadratic_form(p, z_matrix,y_matrix,s_matrix,size_vector,e1_dln_a_index,e1_educ_index);
-
-
-%%
 %Constraining manual to be the same
 A_rest=zeros(2,n_total_parameters);
 A_rest(:,1)=1;
@@ -47,11 +28,21 @@ A_rest(1,5)=-1;
 A_rest(2,9)=-1;
 b_rest=zeros(2,1);
 
-%%
-%Here I import 
+%Importing solution from GMM equation
+load("code/parameter_estimation/restricted_gmm_new.mat",'solution');
+
+initial_sol=solution;
 %%
 
-[solution,MSE]=fmincon(error_solve,initial_sol_gmm,[],[],A_rest,b_rest,lower_bound, upper_bound,[],options);
+clear solution
+
+
+%%
+
+error_solve=@(p)get_quadratic_form(p, z_matrix,y_matrix,s_matrix,size_vector,e1_dln_a_index,e1_educ_index);
+
+
+[solution,MSE]=fmincon(error_solve,initial_sol,[],[],A_rest,b_rest,lower_bound, upper_bound,[],options);
 
 
 %%
